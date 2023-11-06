7ca186015deb14558731cb9186a39f344a154179
Isabella <sorianoisabella28@gmail.com>
2023-11-06 16:25:18 -0500

    implementar el boton de favorito en movie_detail

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:movies_app/models/movie.dart';
+import 'package:movies_app/repositories/favorite_movie_repository.dart';
 
 import '../utils/functions.dart';
 
@@ -16,10 +17,29 @@ class MovieDetail extends StatefulWidget {
 
 class _MovieDetailState extends State<MovieDetail> {
 
+  //atributos
+  bool _favorite=false;
+
+  initialize()async{
+    //_favorite toma el valor que da el metodo del repositorie al pasarle cada entidad
+    _favorite=await FavoriteMovieRepository().isFavorite(widget.movie.id);
+    if (mounted){ //si ya se cargo toda la informacion
+      setState(() {
+        _favorite=_favorite;
+      });
+    }
+  }
+
+  @override
+  void initState(){
+    initialize();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    final my_icon = Icon(Icons.favorite, color: Colors.red);
+    final my_icon = Icon(Icons.favorite, color:_favorite ? Colors.red: Colors.grey);
 
     //imagen del cada movie
     final image = Image.network(getUrl(widget.movie.posterPath));
@@ -44,7 +64,16 @@ class _MovieDetailState extends State<MovieDetail> {
         body: Column(
           children: [
             IconButton(
-                onPressed: (){},
+                onPressed: (){
+                  if(mounted){ //si ya se cargo toda la info
+                    setState(() {
+                      _favorite =! _favorite; //cambia de valor cada que se da click
+                    });
+                  }
+                   //si es true inserta, sino elimina
+                  _favorite? FavoriteMovieRepository().insert(widget.movie.id)
+                  :FavoriteMovieRepository().delete(widget.movie.id);
+                },
                 icon: my_icon
             ),
 
